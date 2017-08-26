c6a0357270eaa2f7fc42cc199280420f71cbf553
Jonathan Backhaus <jonathan.david.backhaus@gmail.com>
2017-08-25 22:17:04 -0400

    Significant updates to update config structure and allow a config file to specified at runtime (via the CLI)

--- speedcomplainer.py
+++ speedcomplainer.py
@@ -1,24 +1,31 @@
 import os
 import sys
+import argparse
 import time
 from datetime import datetime
-import daemon
 import signal
 import threading
 import json
 import random
 from logger import Logger
-# import oauth as OAuth
 from twython import Twython
 
 shutdownFlag = False
 
+def main(filename,argv):
+    global configFile
 
-def main(filename, argv):
-    print "======================================"
-    print " Starting Speed Complainer!           "
-    print " Lets get noisy!                      "
-    print "======================================"
+    arg = argv[0]
+    if os.path.isfile(arg):
+        configFile = arg
+    else:
+        configFile = './config.json'
+
+    print '======================================'
+    print ' Starting Speed Complainer!           '
+    print ' Lets get noisy!                      '
+    print(' Config file: ' + configFile)
+    print '======================================'
 
     global shutdownFlag
     signal.signal(signal.SIGINT, shutdownHandler)
@@ -26,20 +33,19 @@ def main(filename, argv):
     monitor = Monitor()
     time.sleep(10)
     try:
-        print ("*********************##**************************")
-        print("running monitor; time: " + str(datetime.now()))
+        print ('*********************##**************************')
+        print('running monitor; time: ' + str(datetime.now()))
         monitor.run()
-        # print("monitor runtime: ",datetime.now())
+        # print('monitor runtime: ',datetime.now())
         for i in range(0, 6):
             if shutdownFlag:
                 break
             time.sleep(10)
-            # print("click sleep time: ",datetime.now())
-        print ("####################**#######################")
+            # print('click sleep time: ',datetime.now())
+        print ('####################**#######################')
     except Exception as e:
         print 'Error01: %s' % e
         sys.exit(1)
-
     sys.exit()
 
 
@@ -53,37 +59,17 @@ class Monitor():
     def __init__(self):
         self.lastPingCheck = None
         self.lastSpeedTest = None
-        # testing twitter  connection purpose
-        # self.twitterAuth()
-
-    def twitterAuth(self):
-
-        print ("twitter api initialization")
-        self.config = json.load(open('./config.json'))
-        TOKEN = self.config['twitter']['twitterTokenSecret']
-        CON_SEC_KEY = self.config['twitter']['twitterConsumerKey']
-        CON_SEC = self.config['twitter']['twitterConsumerSecret']
-        TOKEN_KEY = self.config['twitter']['twitterToken']
-        twitter = Twython(CON_SEC_KEY, CON_SEC, TOKEN_KEY, TOKEN)
-        twitter.update_status(status='See how easy using Twython is!')
-        # import twitter
-        # my_auth = twitter.OAuth(TOKEN,TOKEN_KEY,CON_SEC,CON_SEC_KEY)
-        # twit = twitter.Twitter(auth=my_auth)
-        # twit.statuses.update(status="I'm tweeting from Python!")
-        # api = twitter.api.Twitter(auth=OAuth(consumer_key=tck,consumer_secret=tcs,access_token_key=ttk,access_token_secret=tts))
-        print ("twitter api initialized")
 
     def run(self):
         if not self.lastPingCheck or (datetime.now() - self.lastPingCheck).total_seconds() >= 60:
             self.runPingTest()
             self.lastPingCheck = datetime.now()
-            print("ping test started")
+            print('ping test started')
 
-        # if not self.lastSpeedTest or (datetime.now() - self.lastSpeedTest).total_seconds() >= 3600:
         if not self.lastSpeedTest or (datetime.now() - self.lastSpeedTest).total_seconds() >= 300:
             self.runSpeedTest()
             self.lastSpeedTest = datetime.now()
-            print("Speed test started\n")
+            print('Speed test started\n')
 
     def runPingTest(self):
         pingThread = PingTest()
@@ -96,41 +82,45 @@ class Monitor():
 
 class PingTest(threading.Thread):
     def __init__(self, numPings=3, pingTimeout=2, maxWaitTime=6):
+        global configFile
         super(PingTest, self).__init__()
         self.numPings = numPings
         self.pingTimeout = pingTimeout
         self.maxWaitTime = maxWaitTime
-        self.config = json.load(open('./config.json'))
-        self.logger = Logger(self.config['log']['type'], {'filename': self.config['log']['files']['ping']})
+        self.config = json.load(open(configFile))
+        assert isinstance(self.config, dict)
+        if 'log' in self.config:
+            self.logger = Logger(self.config['log']['type'], {'filename': self.config['log']['files']['ping']})
 
     def run(self):
         pingResults = self.doPingTest()
-        self.logPingResults(pingResults)
+        if 'log' in self.config:
+            self.logPingResults(pingResults)
 
     def doPingTest(self):
-        print ("[" + str(self.ident) + "] executando ping \n")
+        print ('[' + str(self.ident) + '] executando ping \n')
         # import pdb; pdb.set_trace()
-        if "darwin" in sys.platform:
+        if 'darwin' in sys.platform:
             # macOS response
-            response = os.system("ping -c %s -W %s -w %s 8.8.8.8 > /dev/null 2>&1" % (
-            self.numPings, (self.pingTimeout * 1000), self.maxWaitTime))
-        elif "win" in sys.platform:
+            response = os.system('ping -c %s -W %s -w %s 8.8.8.8 > /dev/null 2>&1' % (
+                self.numPings, (self.pingTimeout * 1000), self.maxWaitTime))
+        elif 'win' in sys.platform:
             # Windows response
-            response = os.system("ping -n %s -w %s www.google.com " % (self.numPings, self.maxWaitTime))
-        elif "linux" in sys.platform:
+            response = os.system('ping -n %s -w %s www.google.com ' % (self.numPings, self.maxWaitTime))
+        elif 'linux' in sys.platform:
             # linux response
-            response = os.system("ping -c %s -W %s -w %s 8.8.8.8 > /dev/null 2>&1" % (
-            self.numPings, (self.pingTimeout * 1000), self.maxWaitTime))
+            response = os.system('ping -c %s -W %s -w %s 8.8.8.8 > /dev/null 2>&1' % (
+                self.numPings, (self.pingTimeout * 1000), self.maxWaitTime))
         else:
-            print("[" + str(self.ident) + "] Sistema nao suportado")
-            print("[" + str(self.ident) + "] " + str(sys.platform))
+            print('[' + str(self.ident) + '] Sistema nao suportado')
+            print('[' + str(self.ident) + '] ' + str(sys.platform))
             response = 1
 
-        print ("[" + (str(self.ident) + "] Response:" + str(response) + "\n"))
+        print ('[' + (str(self.ident) + '] Response:' + str(response) + '\n'))
         success = 0
         if response == 0:
             success = 1
-        print("[" + str(self.ident) + "] success:" + str(success) + "\n")
+        print('[' + str(self.ident) + '] success:' + str(success) + '\n')
         return {'date': datetime.now(), 'success': success}
 
     def logPingResults(self, pingResults):
@@ -139,22 +129,27 @@ class PingTest(threading.Thread):
 
 class SpeedTest(threading.Thread):
     def __init__(self):
+        global configFile
         super(SpeedTest, self).__init__()
-        self.config = json.load(open('./config.json'))
-        self.logger = Logger(self.config['log']['type'], {'filename': self.config['log']['files']['speed']})
+        self.config = json.load(open(configFile))
+        assert isinstance(self.config, dict)
+        if 'log' in self.config:
+            self.logger = Logger(self.config['log']['type'], {'filename': self.config['log']['files']['speed']})
 
     def run(self):
         speedTestResults = self.doSpeedTest()
-        self.logSpeedTestResults(speedTestResults)
-        self.tweetResults(speedTestResults)
+        if 'log' in self.config:
+            self.logSpeedTestResults(speedTestResults)
+        if 'twitter' in self.config:
+            self.tweetResults(speedTestResults)
 
     def doSpeedTest(self):
-        print ("[" + str(self.ident) + "] Starting speedtest \n")
+        print ('[' + str(self.ident) + '] Starting speedtest \n')
         # run a speed test
-        result = os.popen("speedtest-cli --simple").read()
+        result = os.popen('speedtest-cli --simple').read()
         if 'Cannot' in result:
             return {'date': datetime.now(), 'uploadResult': 0, 'downloadResult': 0, 'ping': 0}
-        print "result: " + str(result)
+        print 'result: ' + str(result)
         # Result:
         # Ping: 529.084 ms
         # Download: 0.52 Mbit/s
@@ -167,25 +162,26 @@ class SpeedTest(threading.Thread):
             downloadResult = resultSet[1]
             uploadResult = resultSet[2]
         except Exception as e:
-            pingResult = "Ping: 0.0 ms"
-            downloadResult = "Download: 0.0 Mbit/s"
-            uploadResult = "Upload: 0.0 Mbit/s"
-            print "[" + str(self.ident) + "] ErrorISP Tratado: %s" % e
+            pingResult = 'Ping: 0.0 ms'
+            downloadResult = 'Download: 0.0 Mbit/s'
+            uploadResult = 'Upload: 0.0 Mbit/s'
+            print '[' + str(self.ident) + '] ErrorISP Tratado: %s' % e
 
         pingResult = float(pingResult.replace('Ping: ', '').replace(' ms', ''))
         downloadResult = float(downloadResult.replace('Download: ', '').replace(' Mbit/s', ''))
         uploadResult = float(uploadResult.replace('Upload: ', '').replace(' Mbit/s', ''))
 
-        print ("[" + str(self.ident) + "] Done speedtest")
+        print ('[' + str(self.ident) + '] Done speedtest')
         return {'date': datetime.now(), 'uploadResult': uploadResult, 'downloadResult': downloadResult,
                 'ping': pingResult}
 
     def logSpeedTestResults(self, speedTestResults):
         self.logger.log([speedTestResults['date'].strftime('%Y-%m-%d %H:%M:%S'), str(speedTestResults['uploadResult']),
                          str(speedTestResults['downloadResult']), str(speedTestResults['ping'])])
-        print("[" + str(self.ident) + "] Done log")
+        print('[' + str(self.ident) + '] Done log')
 
     def tweetResults(self, speedTestResults):
+        twitter = self.config['twitter']
         thresholdMessages = self.config['tweetThresholds']
         message = None
         for (threshold, messages) in thresholdMessages.items():
@@ -195,39 +191,24 @@ class SpeedTest(threading.Thread):
                                                                                  self.config['tweetTo']).replace(
                     '{internetSpeed}', self.config['internetSpeed']).replace('{downloadResult}',
                                                                              str(speedTestResults['downloadResult']))
-                print ("[" + str(self.ident) + "] message:", message)
+                print ('[' + str(self.ident) + '] message:', message)
         if message:
-
-            # import twitter
-            # api = twitter.api(consumer_key=self.config['twitter']['twitterConsumerKey'],
-            #                consumer_secret=self.config['twitter']['twitterConsumerSecret'],
-            #                access_token_key=self.config['twitter']['twitterToken'],
-            #                access_token_secret=self.config['twitter']['twitterTokenSecret'])
-            # TOKEN_KEY=self.config['twitter']['twitterTokenSecret']
-            # CON_SEC_KEY=self.config['twitter']['twitterConsumerKey']
-            # CON_SEC=self.config['twitter']['twitterConsumerSecret']
-            # TOKEN=self.config['twitter']['twitterToken']
-            # my_auth = twitter.OAuth(TOKEN,TOKEN_KEY,CON_SEC,CON_SEC_KEY)
-            # twit = twitter.Twitter(auth=my_auth)
-            TOKEN = self.config['twitter']['twitterTokenSecret']
-            CON_SEC_KEY = self.config['twitter']['twitterConsumerKey']
-            CON_SEC = self.config['twitter']['twitterConsumerSecret']
-            TOKEN_KEY = self.config['twitter']['twitterToken']
-            api = Twython(CON_SEC_KEY, CON_SEC, TOKEN_KEY, TOKEN)
+            api = Twython(twitter['consumerKey'],
+                          twitter['consumerSecret'],
+                          twitter['token'],
+                          twitter['tokenSecret'])
 
             if api:
-                # status = api.PostUpdate(message)
-                # twit.statuses.update(status=message)
                 api.update_status(status=message)
             else:
-                print ("[" + str(self.ident) + "] No API")
+                print ('[' + str(self.ident) + '] No API')
         else:
             if speedTestResults['downloadResult'] == 0.0:
-                print("[" + str(
-                    self.ident) + "] speedtest-cli: Mensasen nao enviada do por fala de conectividade ou plataforma nao suportada")
+                print('[' + str(
+                    self.ident) + '] speedtest-cli: Unsupported platform')
             else:
-                print ("[" + str(self.ident) + "] Internet dentro dos padroes estabelecidos")
-        print("[" + str(self.ident) + "] Done tweetResult")
+                print ('[' + str(self.ident) + '] Internet dentro dos padroes estabelecidos')
+        print('[' + str(self.ident) + '] Done tweetResult')
 
 
 class DaemonApp():
